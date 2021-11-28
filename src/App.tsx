2b5bebbb5c1b66a812dc4f458139b6402bb733b6
Jay <nhanlnt@hotmail.com>
2021-11-28 13:49:47 +0700

    Initialize app using create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from 'imgs/logo.png';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="react-logo" />
+      <h1>Hello React</h1>
+      <p>Created by Jay@2021</p>
+    </div>
+  );
+}
+
+export default App;
